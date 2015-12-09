ed2804522538523930ba2864451587903177fce8
Andrey Borodulin <borodulin@gmail.com>
2015-12-09 09:52:01 +0500

    Update Select2Widget.php

--- Select2Widget.php
+++ Select2Widget.php
@@ -76,7 +76,7 @@ class Select2Widget extends \yii\widgets\InputWidget
         parent::init();
         
         if (is_null($this->items) && is_null($this->data) && empty($this->ajax) && empty($this->settings['data'])) {
-            throw new InvalidConfigException('You need to configute one of the data sources');
+            throw new InvalidConfigException('You need to configure one of the data sources');
         }
         if (isset($this->tags)) {
             $this->options['data-tags'] = $this->tags;
